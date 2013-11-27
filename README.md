e0dad13983844f99ca400834dbe4075859403ab5
fortable1999 <fortable1999@rad-xen-vweb5.(none)>
2013-11-27 13:59:15 +0900

    fast image indexer script added

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Zhao's VPS setup scripts
 1. cento64_py2_py3_opencv.sh
   for python2.6 + python2.7 + python3.3 combo
 
+2. Fast Image Indexer
+  Generate a index.html for all images in current directory.
+  This is useful for nginx/apache.
---
+++ fast_image_indexer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+# Fast Image Indexer Document
+# this script generate a index.html for apache/nginx
+# for example:
+#
+# $ ls
+# image1.png image2.jpg
+# 
+# $ python fast_image_indexer.py
+# $ ls index.html
+# index.html
+#
+# Author: fortable1999@gmail.com
+
+import sys
+import os
+import re
+
+TEMPLATE = """
+<html>
+<title>Fast Image Indexer</title>
+<body>
+<div align="center">
+%s
+</div>
+</body>
+</html>
+"""
+IMG_TEMPLATE = """
+<img src="%s" align="middle"></img>
+"""
+
+def render(images):
+	img_html = ""
+	for img in images:
+		img_html += IMG_TEMPLATE % img
+	
+	return TEMPLATE % img_html
+
+def image_finder():
+	res = []
+	for f in os.listdir('.'):
+		if re.search(r'\.(bmp|jpg|png|jpeg)$', f):
+			print type(f)
+			res.append(f)
+	return res
+
+if __name__ == '__main__':
+	html = open("index.html", 'w')
+	html.write(render(image_finder()))
