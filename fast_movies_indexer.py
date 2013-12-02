1d1cb62c6cf00b8aa753c9e163451cae848c408b
fortable1999 <fortable1999@gmail.com>
2013-12-02 19:33:13 +0900

    add movie version of fast indexer

---
+++ fast_movies_indexer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Fast Movie Indexer Document
+# this script generate a index.html for apache/nginx
+# for example:
+#
+# $ ls
+# helloworld.mp4
+# 
+# $ python fast_image_indexer.py
+# $ ls index.html
+# index.html
+#
+# Author: fortable1999@gmail.com
+
+import sys
+import os
+import re
+
+TEMPLATE = """
+<html>
+<title>Fast Movie Indexer</title>
+<body>
+<div align="center">
+<h1>Fast Movie Indexer</h1>
+<hr>
+%s
+</div>
+</body>
+</html>
+"""
+MOV_TEMPLATE = """
+<h3>"{0}"</h3>
+<br/>
+<video width="80%" controls>
+  <source src="{1}" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+<hr>
+"""
+
+def render(movies):
+    mov_html = ""
+    for movie in movies:
+        mov_html += MOV_TEMPLATE.format(movie, movie)
+    return TEMPLATE % mov_html
+
+def movie_finder():
+    res = []
+    for f in sorted(os.listdir('.')):
+        if re.search(r'\.(mp4)$', f):
+            res.append(f)
+    return res
+
+if __name__ == '__main__':
+    with open("index.html", 'w') as html:
+        html.write(render(movie_finder()))
